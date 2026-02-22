35eaab57a4e43d6778bf6cb781f3936407f2597a
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-02-22 16:50:12 +0100

    Update info files

--- pkgs/string-tools-lib/info.rkt
+++ pkgs/string-tools-lib/info.rkt
@@ -1,11 +1,11 @@
 #lang info
 
-(define collection "string-tools")
-(define deps '("base"))
-(define build-deps '("rackunit-lib" "scribble-lib"))
+(define collection  "string-tools")
+(define deps        '("base"))
+(define build-deps  '("rackunit-lib" "scribble-lib"))
 (define scribblings '(("string-tools-lib.scrbl" () (library))))
-(define pkg-desc "Library of practical string and character-set utilities.")
-(define pkg-tags '("string" "strings" "text" "charset"))
-(define version "0.1")
-(define license 'MIT)
+(define pkg-desc    "Library of practical string and character-set utilities.")
+(define pkg-tags    '("string" "strings" "text" "charset"))
+(define version     "0.1")
+(define license     'MIT)
 (define pkg-authors '("Jens Axel Søgaard"))
