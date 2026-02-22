fbdb49092c71535a813dcb46cc5e248294322f26
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-02-22 12:26:24 +0100

    Fix dependcies

--- pkgs/string-tools/info.rkt
+++ pkgs/string-tools/info.rkt
@@ -1,9 +1,8 @@
 #lang info
 
-(define deps '("base" "string-tools-lib"))
-(define build-deps '("scribble-lib"))
+(define deps '("base" "scribble-lib" "string-tools-lib"))
+(define build-deps '("racket-doc"))
 (define scribblings '(("string-tools.scrbl" (multi-page) (library))))
 (define pkg-desc "Documentation for string-tools")
 (define version "0.1")
 (define pkg-authors '("Jens Axel Søgaard"))
-
